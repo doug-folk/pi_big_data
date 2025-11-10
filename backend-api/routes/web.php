c025203881dc439fff755c04aa20644040c741cd
Antonio Kayky G V. <kitzyoff@gmail.com>
2025-11-10 18:10:23 -0300

    Update web.php

--- backend-api/routes/web.php
+++ backend-api/routes/web.php
@@ -2,8 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    $frontendUrl = config('app.frontend_url', 'http://localhost:3000');
-
-    return redirect()->away($frontendUrl);
-});
+Route::get('/{any}', function () {
+    return file_get_contents(public_path('index.html'));
+})->where('any', '.*');
